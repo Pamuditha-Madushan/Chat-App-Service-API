da7fc9462d3eb13b88cc70c870d803b213277b7d
Pramuditha De Silva <gadse191f-053@student.nibm.lk>
2024-03-13 23:20:24 +0530

    Initial commit

---
+++ controller/imageController.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const generateDevelopersStackResourceName = require("../config/genaret");
+const ImageDetails = require("../models/imageDetials");
+
+const s3Client = new S3Client({
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY
+    },
+    region: 'ap-south-1' });
+
+const fs = require('fs');
+
+const uploadImage = async (req, res) => {
+    const file = req.file;
+
+    const data = JSON.parse(req.body.data);
+    console.log(data.name)
+    const name=data.name;
+    const age=data.age;
+    const fileData = fs.readFileSync(file.path);
+    const fileName = generateDevelopersStackResourceName(
+        file.originalname,
+        "P",
+        20
+    );
+
+    const params = {
+        Bucket: 'sample-bucket-service-3',
+        Key: `ozh-hub-sample/business/file/${fileName}`,
+        Body: fileData,
+        ContentType: file.mimetype,
+        ACL: 'public-read',
+    };
+    try {
+        const command = new PutObjectCommand(params);
+        const response = await s3Client.send(command);
+        fs.unlinkSync(file.path);
+        const fileUrl = `https://sample-bucket-service-3.s3.ap-south-1.amazonaws.com/ozh-hub-sample/business/file/${fileName}`;
+        console.log(fileUrl)
+
+        const image = new ImageDetails({
+            name,
+            age,
+            imageUrl: fileUrl
+        });
+
+        // Save the image details to the database
+        await image.save();
+        console.log('File uploaded successfully:', response);
+        res.status(200).json({ message: 'File uploaded successfully' });
+    } catch (err) {
+        console.error('Error uploading file:', err);
+        res.status(500).json({ error: 'Failed to upload file to AWS S3' });
+    }
+};
+
+
+module.exports = {uploadImage};
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv")
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000
+const server = http.createServer(app);
+const colors = require("colors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes")
+const messageRoutes = require("./routes/messageRoutes")
+const imageRoutes=require("./routes/imageRoutes")
+//const AWS = require('aws-sdk');
+
+const {notFound, errorHandler} = require("./middlewares/errorMiddlewares");
+
+app.use(express.json());
+
+connectDB();
+
+app.get("/check", (req, res) => {
+    res.send("Api was running");
+})
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/image",imageRoutes)
+
+
+app.use(notFound);
+app.use(errorHandler);
+const listenServer =app.listen(port, console.log(`Server started on ${port}`))
+const io = require("socket.io")(listenServer, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "https://mern-chat-app-e6zt.onrender.com",
+        credentials: true
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach((user) => {
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message received", newMessageReceived);
+        });
+    });
+
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    });
+});
