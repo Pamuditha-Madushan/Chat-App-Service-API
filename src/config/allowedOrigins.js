3dedf2bf063de5ccfc935ded1a785605d8a5e0cb
Pamuditha Madushan <pramudithadesilva00@gmail.com>
2025-02-25 20:15:19 +0530

    added deployment url to allowed origins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://127.0.0.1:3500", "http://localhost:3500"];
+const allowedOrigins = [
+  "http://127.0.0.1:3500",
+  "http://localhost:3500",
+  "https://real-time-chat-web-app-backend.onrender.com",
+];
 
 export default allowedOrigins;
